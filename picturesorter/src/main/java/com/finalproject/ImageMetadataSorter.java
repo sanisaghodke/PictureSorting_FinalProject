29e914e1e7f2a7d9d1b75c0aff9a3999ad04d1c0
Sanisa Ghodke <sghodke.dogsrock@gmail.com>
2026-05-23 21:47:44 -0700

    extracted date from metadata

--- picturesorter/src/main/java/com/finalproject/ImageMetadataSorter.java
+++ picturesorter/src/main/java/com/finalproject/ImageMetadataSorter.java
@@ -1,13 +1,31 @@
 package com.finalproject;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.Date;
+
+import com.drew.imaging.ImageMetadataReader;
+import com.drew.imaging.ImageProcessingException;
+import com.drew.metadata.Directory;
+import com.drew.metadata.Metadata;
+import com.drew.metadata.Tag;
 
 public class ImageMetadataSorter{
+    //iterates through the directory and lists the files, if it is a folder it goes into it and repeats the process. 
     public static void processFiles(File[] files){
         for (File file : files) {
             if (file.isFile()) {
                 // Process the file
                 System.out.println("Processing file: " + file.getName());
+                try {
+                    Metadata metadata = ImageMetadataReader.readMetadata(file);
+                    System.out.println(metadata);
+                    print(metadata);
+                } catch (ImageProcessingException e) {
+                    System.out.println(e);
+                } catch (IOException e) {
+                    System.out.println(e);
+                }
             } else if (file.isDirectory()) {
                 // Recursively process the directory
                 processFiles(file.listFiles());
@@ -18,10 +36,28 @@ public class ImageMetadataSorter{
         // Storing the name of files and directories
         // in an array of File type
         File[] files = new File(inputDirectory).listFiles();
-
-        // Calling method 1 to
-        // display files
         processFiles(files);
+    }
+    private static void print(Metadata metadata){
+
+        //
+        // A Metadata object contains multiple Directory objects
+        //
+        for (Directory directory : metadata.getDirectories()) {
+
+            //
+            // Each Directory stores values in Tag objects
+            //
+            for (Tag tag : directory.getTags()) {
+                if(tag.getTagName().contains("Date")){
+                    System.out.println(tag.getDescription());
+                    Date date = directory.getDate(tag.getTagType());
+                    System.out.println(date.getYear()+1900); // Date.getYear() returns years since 1900
+                    System.out.println(date.getMonth()+1); // Date.getMonth() returns months from 0-11
+                    System.out.println(date.getDate());
+                }
+            }
+        }
     }
 }
 
